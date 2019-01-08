e32c9c8c489e03080517824c7e0f2fd4432dafa9
Mazi <rafalek.mazur@gmail.com>
2019-01-08 14:54:06 +0100

    Dodane GUI

--- src/kolokwium/Controller.java
+++ src/kolokwium/Controller.java
@@ -1,44 +1,102 @@
 package kolokwium;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 
 public class Controller {
 
-    @FXML private TextField textfieldFindByAuthor;
-    @FXML private TextField textfieldFindByISBN;
+    @FXML private TextField tile00;
+    @FXML private TextField tile01;
+    @FXML private TextField tile02;
 
-    @FXML private TextField textfieldAddISBN;
-    @FXML private TextField textfieldAddTitle;
-    @FXML private TextField textfieldAddAuthor;
-    @FXML private TextField textfieldAddYear;
+    @FXML private TextField tile10;
+    @FXML private TextField tile11;
+    @FXML private TextField tile12;
 
-    @FXML private TextField textfieldDeleteByISBN;
-
-
-    @FXML private TableView<Player> tableView;
-    @FXML private TableColumn<Player, String> col1;
-    @FXML private TableColumn<Player, String> col2;
-    @FXML private TableColumn<Player, String> col3;
-    @FXML private TableColumn<Player, String> col4;
+    @FXML private TextField tile20;
+    @FXML private TextField tile21;
+    @FXML private TextField tile22;
 
     private EchoClient echoClient = new EchoClient();
 
-    @FXML
-    public void bClear(){
-        tableView.getItems().clear();
+    short player=0;
+    short count=0;
+
+    private void changeValueOfTile(TextField tile)
+    {
+        if(tile.getText()=="") {
+            if (player == 0) {
+                player = 1;
+                tile.setText("X");
+            } else {
+                player = 0;
+                tile.setText("O");
+            }
+            if(++count==9)
+                sendMessageToServer();
+        }
+    }
+
+    private void sendMessageToServer()
+    {
+        //Zawsze takie samo zapytanie aby pokazac że baza danych działa
+        String query ="INSERT INTO players VALUES('"+"player1"+"','"+"1"+"0"+"player2"+"')";
+        echoClient.addPlayerToDataBase(query);
     }
 
     @FXML
-    public void addBookWhenButtonIsClick()
+    public void handletile00()
     {
-        String query ="INSERT INTO players VALUES('"+player.getName()+"','" +player.getTest()+"')";
-        echoClient.addPlayer();
+        changeValueOfTile(tile00);
+    }
+
+    @FXML
+    public void handletile01()
+    {
+        changeValueOfTile(tile01);
+    }
+
+    @FXML
+    public void handletile02()
+    {
+        changeValueOfTile(tile02);
+    }
+
+    @FXML
+    public void handletile10()
+    {
+        changeValueOfTile(tile10);
+    }
+
+    @FXML
+    public void handletile11()
+    {
+        changeValueOfTile(tile11);
+    }
+
+    @FXML
+    public void handletile12()
+    {
+        changeValueOfTile(tile12);
+    }
+
+    @FXML
+    public void handletile20()
+    {
+        changeValueOfTile(tile20);
+    }
+
+    @FXML
+    public void handletile21()
+    {
+        changeValueOfTile(tile21);
+    }
+
+    @FXML
+    public void handletile22()
+    {
+        changeValueOfTile(tile22);
     }
 
 }
--- src/kolokwium/EchoClientTEST.java
+++ src/kolokwium/EchoClientTEST.java
@@ -1,9 +1,22 @@
 package kolokwium;
 
-public class EchoClientTEST {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EchoClientTEST extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TicTacToe.fxml"));
+        stage.setTitle("DB");
+        stage.setScene(new Scene(root,1024,400));
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        EchoClient echoClient = new EchoClient();
-        String query ="INSERT INTO players VALUES('"+"aaaaa"+"','" +"bbbb"+"')";
-        echoClient.addPlayerToDataBase(query);
+        launch(args);
     }
 }
+
